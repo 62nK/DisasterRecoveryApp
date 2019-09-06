b9cae8cb7921f4551d9e8bb049fc14239fb34547
Guido Heredia <herediaguido2@gmail.com>
2019-09-06 10:25:20 -0800

    git commit -m "machine-code-managements changes"

--- src/app/machine-code-management/machine-code-management.component.ts
+++ src/app/machine-code-management/machine-code-management.component.ts
@@ -1,19 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { MachineCodeService } from '../shared/services/machine-code.service';
+import { MachineCode } from '../shared/models/machinecode';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-machine-code-management',
   templateUrl: './machine-code-management.component.html',
   styleUrls: ['./machine-code-management.component.css']
 })
-export class MachineCodeManagementComponent implements OnInit {
+export class MachineCodeManagementComponent implements OnInit, DoCheck {
 
   componentTitle: string;
+  machineCodeList: Array<MachineCode>;
+  errorMessage: string;
 
-  constructor() { 
+  constructor(private _machineCodeService: MachineCodeService, private _router: Router) { 
     this.componentTitle = "Machine Code Management";
   }
 
   ngOnInit() {
+    this.getMachineCodeList();
+  }
+
+  addNewMachineCode(){
+    this._router.navigate(["home/machinecode/submission"]);
+  }
+
+  updateNewMachineCode(machineCode: MachineCode){
+    this._router.navigate(["home/machinecode/submission"]);
+  }
+
+  removeMachineCode(machineCode: MachineCode){
+    this._machineCodeService.removeMachineCode(machineCode).subscribe(
+      (response)=>{
+        console.log(response);
+        this.getMachineCodeList();
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+  }
+
+  getMachineCodeList(){
+    this._machineCodeService.getMachineCodeList().subscribe(
+      (machineCodeList)=>{
+        this.machineCodeList = machineCodeList;
+      },
+      (error)=>this.errorMessage = error
+    );
+  }
+
+  ngDoCheck(): void {
+
   }
 
 }
