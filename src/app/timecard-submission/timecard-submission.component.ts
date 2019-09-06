3d60c1700a3881ccfc6054cd13c61afeebd0dd45
Andrea Pinardi <andreapinardi39@gmail.com>
2019-09-06 01:12:33 -0400

    fixed routerlink, updated timecard submission

--- src/app/timecard-submission/timecard-submission.component.ts
+++ src/app/timecard-submission/timecard-submission.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, FormArray, FormArrayName } from '@angular/forms';
 
 @Component({
   selector: 'app-timecard-submission',
@@ -10,16 +10,43 @@ export class TimecardSubmissionComponent implements OnInit {
 
   timeCardSubmissionForm: FormGroup;
 
-  constructor() { 
-    this.timeCardSubmissionForm = new FormGroup({
-      code: new FormControl(),
-      contractorName: new FormControl(),
-      date : new FormControl(),
-      laborEntry : new FormGroup({
-        entry: new FormControl(),
+  constructor(private _formBuilder: FormBuilder) { 
+
+    this.timeCardSubmissionForm = _formBuilder.group({
+      details: _formBuilder.group({
+        code: [],
+        contractorName: [],
+        date: []
       }),
+      laborEntries: this._formBuilder.array([this.createLaborEntry()]),
+      machineEntries: this._formBuilder.array([this.createMachineEntry()])
     });
   }
+
+  addMachineEntry() {
+    const machineEntries = this.timeCardSubmissionForm.get('machineEntries') as FormArray;
+    machineEntries.push(this.createMachineEntry());
+  }
+
+  addLaborEntry() {
+    const laborEntries = this.timeCardSubmissionForm.get('laborEntries') as FormArray;
+    laborEntries.push(this.createLaborEntry());
+  }
+
+  createLaborEntry(): FormGroup {
+    return this._formBuilder.group({
+        code: [],
+        hoursWorked: [],
+        total: []
+    });
+  }
+  createMachineEntry(): FormGroup {
+    return this._formBuilder.group({
+      code: [],
+      hoursUsed: [],
+      total: []
+  });
+}
 
   ngOnInit() {
   }
