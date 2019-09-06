747982486eaaa43dfceb30b587604076b8c67e7e
Guido Heredia <herediaguido2@gmail.com>
2019-09-06 11:01:50 -0800

    mc submission component changed

--- src/app/machine-code-submission/machine-code-submission.component.ts
+++ src/app/machine-code-submission/machine-code-submission.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MachineCodeService } from '../shared/services/machine-code.service';
+import { MachineCode } from '../shared/models/machinecode';
+import { Router } from '@angular/router';
+import { JobCodeService } from '../shared/services/job-code.service';
 
 @Component({
   selector: 'app-machine-code-submission',
@@ -7,8 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MachineCodeSubmissionComponent implements OnInit {
 
-  constructor() { }
+  componentTitle: string;
+  machineCodeSubmissionForm: FormGroup;
+  message: string;
 
+  constructor(private _machineCodeService: MachineCodeService, private router: Router) {
+    this.componentTitle = "Machine Code Submission";
+    this.machineCodeSubmissionForm =new FormGroup({
+      code: new FormControl("", Validators.required),
+      description: new FormControl(),
+      hourlyRent: new FormControl("", Validators.required),
+      maxDailyHours: new FormControl("", Validators.required)
+    });
+   }
+
+   onSubmit(){
+     let machineCode = new MachineCode(
+       this.machineCodeSubmissionForm.value.code,
+       this.machineCodeSubmissionForm.value.description,
+       this.machineCodeSubmissionForm.value.hourlyRent,
+       this.machineCodeSubmissionForm.value.maxDailyHours
+     );
+     this._machineCodeService.createMachineCode(machineCode).subscribe(
+       (response)=>{
+         this.router.navigate(["home/machinecode/management"]);
+       },
+       (error)=>console.log(error)
+     );
+   }
   ngOnInit() {
   }
 
